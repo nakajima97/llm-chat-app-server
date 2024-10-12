b374448c389774dc86ab6a168eadd2f0db5ffdc6
Yudai Nakajima <kituneudon97@gmail.com>
2024-10-12 18:55:10 +0900

    #1 レスポンスが返ってくることを確認

--- api/src/routers/chat.py
+++ api/src/routers/chat.py
@@ -1,10 +1,16 @@
 from fastapi import APIRouter, Depends
 
+from src.schemes.chat import ChatRequest
+from src.usecases.chat_gpt.chat import chat_gpt
+
 router = APIRouter()
 
 @router.get("/chat", tags=["chat"])
-async def get_chat():
-    ```
-    Chat GPTとのチャットを行う
-    ```
-    return {"chat": "chat"}
+async def get_chat(request=Depends(ChatRequest)):
+    '''
+    チャットを取得する
+    '''
+    text = request.text
+    message = chat_gpt(text)
+
+    return {"chat": message}
--- api/src/usecases/chat_gpt/chat.py
+++ api/src/usecases/chat_gpt/chat.py
@@ -7,26 +7,23 @@ from langchain_openai import ChatOpenAI
 load_dotenv()
 OPENAI_API_KEY = os.environ.get("OPENAI_API_KEY")
 
-# OpenAIのモデルのインスタンスを作成
-llm = ChatOpenAI(openai_api_key=OPENAI_API_KEY, model_name="gpt-4o-mini", temperature=0)
+def chat_gpt(sentence):
+    '''
+    OpenAIのGPT-4o-miniを使って文章を生成する
+    '''
+    # OpenAIのモデルのインスタンスを作成
+    llm = ChatOpenAI(openai_api_key=OPENAI_API_KEY, model_name="gpt-4o-mini", temperature=0)
 
-# プロンプトのテンプレート文章を定義
-template = """
-次の文章に誤字がないか調べて。誤字があれば訂正してください。
-{sentences_before_check}
-"""
+    # チャットメッセージを文字列に変換するための出力解析インスタンスを作成
+    output_parser = StrOutputParser()
 
-# テンプレート文章にあるチェック対象の単語を変数化
-prompt = ChatPromptTemplate.from_messages([
-    ("system", "あなたは優秀な校正者です。"),
-    ("user", template)
-])
+    # プロンプトの生成
+    prompt = ChatPromptTemplate.from_messages([
+        ("user", sentence)
+    ])
 
-# チャットメッセージを文字列に変換するための出力解析インスタンスを作成
-output_parser = StrOutputParser()
+    # OpenAIのAPIにこのプロンプトを送信するためのチェーンを作成
+    chain = prompt | llm | output_parser
 
-# OpenAIのAPIにこのプロンプトを送信するためのチェーンを作成
-chain = prompt | llm | output_parser
-
-# チェーンを実行し、結果を表示
-print(chain.invoke({"sentences_before_check": "こんんんちわ、真純です。"}))
+    # チェーンを実行し、結果を返す
+    return chain.invoke({})
