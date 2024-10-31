f1b29155d2ffd238b3d6224aebb40bf0cd49c4d8
Nakajima <kituneudon97@gmail.com>
2024-10-31 11:01:22 +0900

    CORSを設定した (#12)
    * #11 ミドルウェアの設定を追加
    
    * #11 リンターの実行

--- api/src/main.py
+++ api/src/main.py
@@ -1,7 +1,10 @@
 from fastapi import FastAPI
 from src.routers import chat
+from src.middleware import add_cors_middleware
 
 app = FastAPI()
+
+add_cors_middleware(app)
 
 app.include_router(chat.router)
 
---
+++ api/src/middleware.py
@@ -0,0 +1,18 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+
+def add_cors_middleware(app: FastAPI):
+    # 許可するオリジンのリスト
+    origins = [
+        "http://localhost:3000",
+    ]
+
+    # CORSミドルウェアを追加
+    app.add_middleware(
+        CORSMiddleware,
+        allow_origins=origins,  # 許可するオリジン
+        allow_credentials=True,
+        allow_methods=["*"],  # 許可するHTTPメソッド
+        allow_headers=["*"],  # 許可するHTTPヘッダー
+    )
