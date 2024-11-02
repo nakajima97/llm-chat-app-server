824e42e21694135926924ca5f28e93092699772c
Nakajima <kituneudon97@gmail.com>
2024-11-02 11:13:12 +0900

    CORSのオリジンを.envから取得するようにした (#14)
    * #13 CORSのオリジンを.envから取得するようにした
    
    * #13 サンプルの追加
    
    * #13 フォーマッターの実行
    
    * #13 テストで参照するgithub actionsの修正

--- api/src/middleware.py
+++ api/src/middleware.py
@@ -1,12 +1,15 @@
+import os
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from dotenv import load_dotenv
+
+load_dotenv()
+CORS_ORIGIN = os.environ.get("CORS_ORIGIN")
 
 
 def add_cors_middleware(app: FastAPI):
     # 許可するオリジンのリスト
-    origins = [
-        "http://localhost:3000",
-    ]
+    origins = CORS_ORIGIN.split(",")
 
     # CORSミドルウェアを追加
     app.add_middleware(
