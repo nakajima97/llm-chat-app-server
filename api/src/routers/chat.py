5a0a826eebec4b95aed9d455358e986644786038
nakajima97 <kituneudon97@gmail.com>
2024-11-04 11:24:41 +0900

    #20 リファクタリング

--- api/src/routers/chat.py
+++ api/src/routers/chat.py
@@ -3,7 +3,7 @@ from fastapi.responses import StreamingResponse
 from sqlalchemy.ext.asyncio import AsyncSession
 from src.schemes.chat import ChatRequest
 from src.usecases.chat_gpt.chat import chat_gpt
-from src.usecases.chat_gpt.sse import stream_generate
+from src.usecases.chat_gpt.sse import stream_generator
 from src.db import get_db
 from src.usecases.chat_history.save import save_chat_history
 
@@ -31,14 +31,15 @@ async def get_chat_sse(request=Depends(ChatRequest), db: AsyncSession = Depends(
     """
     text = request.text
     chat_room_id = request.chat_room_id
-    stream = stream_generate(text)
+    stream = stream_generator(text)
 
+    # DBにチャット履歴を保存するためにstreamをイベントジェネレータに変換
     async def event_generator():
         response = ""
         async for chunk in stream:
             response += chunk
             yield chunk
-        # Save chat history after streaming is done
+        # streamが終了したらDBにチャット履歴を保存
         await save_chat_history(db, chat_room_id, text, response)
 
     return StreamingResponse(event_generator(), media_type="text/plain")
