55c77718059e8a2f5820651e6a6bc6276bcdbbd5
nakajima97 <kituneudon97@gmail.com>
2024-11-08 08:57:05 +0900

    #22 DBから値を取得できることを確認

--- api/src/routers/threads.py
+++ api/src/routers/threads.py
@@ -1,8 +1,10 @@
 from fastapi import APIRouter, Depends
 from sqlalchemy.ext.asyncio import AsyncSession
+import uuid
 
 from src.db import get_db
 from src.usecases.chat_thread.read import read_chat_threads
+from src.usecases.chat_thread.get_chat_messages_by_thread_id import get_chat_messages_by_thread_id
 
 router = APIRouter(
     prefix="/threads",
@@ -13,4 +15,9 @@ router = APIRouter(
 @router.get("/")
 async def get_threads(db: AsyncSession = Depends(get_db)):
     threads = await read_chat_threads(db)
+    return threads
+
+@router.get("/{thread_id}")
+async def get_thread(thread_id: uuid.UUID, db: AsyncSession = Depends(get_db)):
+    threads = await get_chat_messages_by_thread_id(db, thread_id)
     return threads
