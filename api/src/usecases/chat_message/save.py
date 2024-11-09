5471604d9ea49885be1681debffeb804378ac176
nakajima97 <kituneudon97@gmail.com>
2024-11-09 10:52:31 +0900

    #39 lint errorの解消

--- api/src/usecases/chat_message/save.py
+++ api/src/usecases/chat_message/save.py
@@ -1,19 +1,16 @@
+import uuid
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from src.repositories.chat_message import create_chat_message
 from src.constants import ChatRoleId
-from src.usecases.chat_thread.generate_title import generate_title
 
 
 async def save_chat_message(
-    db: AsyncSession, chat_thread_id: int | None, text: str, message: str
+    db: AsyncSession, chat_thread_id: uuid.UUID, text: str, message: str
 ) -> None:
     """
     チャット履歴を保存する
     """
-    if not chat_thread_id:
-        chat_thread_id = await generate_thread(db, chat_thread_id, text, message)
-
     # ユーザの質問を保存
     await create_chat_message(db, chat_thread_id, text, ChatRoleId.USER)
 
