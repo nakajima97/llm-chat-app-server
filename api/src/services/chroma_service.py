5cf9b764dde729b3b79d1e17f1195f86932fcbda
nakajima97 <kituneudon97@gmail.com>
2024-12-20 18:20:09 +0900

    共通処理を切り出した

---
+++ api/src/services/chroma_service.py
@@ -0,0 +1,19 @@
+from langchain_openai import OpenAIEmbeddings
+from chromadb import HttpClient
+from src.config import Config
+from langchain_chroma import Chroma
+
+def get_chroma_client():
+    embeddings = OpenAIEmbeddings(model="text-embedding-3-large")
+
+    vector_store = Chroma(
+        collection_name="example_collection",
+        embedding_function=embeddings,
+        client=HttpClient(
+            host=Config.CHROMA_HOST,
+            port=Config.CHROMA_PORT,
+            ssl=Config.CHROMA_SSL.lower() == "true",
+        ),
+    )
+
+    return vector_store
