79b9fba614cb90cc4021b9ccb09e6e7c03fb5933
nakajima97 <kituneudon97@gmail.com>
2025-02-20 09:15:47 +0900

    #73 configs.configはややこしいのでより適切なEnvに修正

--- api/src/clients/openai_client.py
+++ api/src/clients/openai_client.py
@@ -1,8 +1,8 @@
 from langchain_openai import ChatOpenAI
-from src.configs.config import Config
+from src.configs.env import Env
 
 
 def get_openai_client():
     return ChatOpenAI(
-        openai_api_key=Config.OPENAI_API_KEY, model_name="gpt-4o-mini", temperature=0
+        openai_api_key=Env.OPENAI_API_KEY, model_name="gpt-4o-mini", temperature=0
     )
